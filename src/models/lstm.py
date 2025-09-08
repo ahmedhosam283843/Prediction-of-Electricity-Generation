82e8a66d2899848f48756f3fa0013a771e118a51
ahmedhosam283843 <ahmed.hosam283843@gmail.com>
2025-09-08 14:45:15 +0300

    update the LSTM architecture

--- src/models/lstm.py
+++ src/models/lstm.py
@@ -4,12 +4,12 @@ import torch.nn as nn
 class LSTMForecaster(nn.Module):
     def __init__(self, input_dim, lookback, horizon):
         super().__init__()
-        self.lstm1 = nn.LSTM(input_size=input_dim, hidden_size=96, batch_first=True)
-        self.lstm2 = nn.LSTM(input_size=96, hidden_size=64, batch_first=True)
-        self.fc1 = nn.Linear(64, 64)
-        self.dropout = nn.Dropout(0.2)
+        self.lstm1 = nn.LSTM(input_size=input_dim, hidden_size=256, batch_first=True)
+        self.lstm2 = nn.LSTM(input_size=256, hidden_size=128, batch_first=True)
+        self.fc1 = nn.Linear(128, 128)
+        self.dropout = nn.Dropout(0.3)
         self.dropout2 = nn.Dropout(0.1)
-        self.fc_out = nn.Linear(64, horizon)
+        self.fc_out = nn.Linear(128, horizon)
 
     def forward(self, x):
         x, _ = self.lstm1(x)
